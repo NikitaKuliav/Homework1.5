ec0f7dc4cc87293c5adf3e3382121108de16312a
Nikita <athlon64x2600@gmail.com>
2022-06-01 18:03:12 +0200

    // Задача 4. В результате в консоль должно быть выведено "Ivanov Ivan".

--- src/Main.java
+++ src/Main.java
@@ -48,5 +48,14 @@ public class Main {
         System.out.println();
         System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
 
+        // Задача 4
+
+        System.out.println();
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
