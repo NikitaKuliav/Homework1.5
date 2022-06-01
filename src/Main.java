8b40adf8f4132be94f30e2fb6b5bed721aaa5a2b
Nikita <athlon64x2600@gmail.com>
2022-06-01 17:06:57 +0200

    // Задача 1. Посчитать сумму всех выплат за месяц.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+
+    // Генератор массивов с размером 30 и данными от 100_000 до 200_000
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 1
+
+        int[] arr = generateRandomArray();
+        int sum = 0;
+
+        for (int j : arr) {
+            sum += j;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+
     }
 }
